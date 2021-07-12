4ed7a7518c5ff9e1061c3a4376feca1ec3d3c39a
Ikmal Azman <opikmalazman@gmail.com>
2021-07-12 21:36:52 +0800

    adjust port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middleware = jsonServer.defaults();
-const port = process.env.port || 3000;
+const port = process.env.PORT;
 
 server.use(middleware);
 server.use(router);
